d86a3985b232653b2901d794e908bada36469918
Sohel <152502004+5OH3L@users.noreply.github.com>
2025-08-23 22:52:41 +0530

    Code gameboard factory function & its tests

--- battleship.js
+++ battleship.js
@@ -13,14 +13,83 @@ function Ship(len) {
       return hits;
     },
     hit() {
-      return hits >= length ? hits : ++hits;
+      if (sunk) return hits;
+      if (++hits >= length) sunk = true;
+      return hits;
     },
     get sunk() {
-      if (hits >= length) sunk = true;
-      else sunk = false;
       return sunk;
     },
   };
 }
 
-export { Ship };
+function Gameboard() {
+  // 0 = haven't hit
+  // 1,2,3,4,5 = ship's index that is on the grid
+  // -1 = shot an empty cell
+  // -2 = shot a ship
+  const ships = [Ship(2), Ship(3), Ship(3), Ship(4), Ship(5)];
+  const board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  const missedShots = [];
+  function areAllShipsSunk() {
+    return ships.every(ship => ship.sunk === true);
+  }
+  function receiveAttack(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) throw new Error("Coordinates must be integers!");
+    if (x > 9 || x < 0 || y > 9 || y < 0) throw new Error("Invalid coordinates!");
+    if (board[x][y] === 0) {
+      board[x][y] = -1;
+      missedShots.push([x, y]);
+    } else {
+      switch (board[x][y]) {
+        case 1:
+          ships[0].hit();
+          break;
+        case 2:
+          ships[1].hit();
+          break;
+        case 3:
+          ships[2].hit();
+          break;
+        case 4:
+          ships[3].hit();
+          break;
+        case 5:
+          ships[4].hit();
+          break;
+      }
+      board[x][y] = -2;
+    }
+    if (areAllShipsSunk()) {
+      console.info("All ships sunk");
+    }
+  }
+  return {
+    get board() {
+      return board;
+    },
+    get ships() {
+      return ships;
+    },
+    receiveAttack,
+    get missedShots() {
+      return missedShots;
+    },
+    get allShipsSunk() {
+      return areAllShipsSunk();
+    },
+  };
+}
+
+export { Ship, Gameboard };
--- battleship.test.js
+++ battleship.test.js
@@ -1,4 +1,4 @@
-import { Ship } from "./battleship.js";
+import { Ship, Gameboard } from "./battleship.js";
 
 describe("Ship", () => {
   it("Can't create ship without providing length", () => {
@@ -46,5 +46,89 @@ describe("Ship", () => {
     ship.hit();
     ship.hit();
     expect(ship.sunk).toBeTruthy();
+  });
+});
+
+describe("Gameboard", () => {
+  let gameboard;
+  beforeEach(() => {
+    gameboard = Gameboard();
+  });
+  it("Initially every cell shouldn't be hit", () => {
+    expect(gameboard.board.every(row => row.every(cell => cell === 0))).toBeTruthy();
+  });
+  it("Throw error for invalid coordinates", () => {
+    expect(() => gameboard.receiveAttack(10, -1)).toThrow("Invalid coordinates!");
+  });
+  it("Throw error for non integer coordinates", () => {
+    expect(() => gameboard.receiveAttack("9", 9)).toThrow("Coordinates must be integers!");
+    expect(() => gameboard.receiveAttack(4, "5")).toThrow("Coordinates must be integers!");
+    expect(() => gameboard.receiveAttack("-1", "10")).toThrow("Coordinates must be integers!");
+  });
+  it("Able to hit an empty cell", () => {
+    gameboard.receiveAttack(4, 4);
+    expect(gameboard.board[4][4]).toBe(-1);
+  });
+  it("Able to hit a ship", () => {
+    // Simulate first ship in coordinates [3,3]
+    gameboard.board[3][3] = 1;
+    gameboard.receiveAttack(3, 3);
+    expect(gameboard.board[3][3]).toBe(-2);
+  });
+  it("Ship hits increased after a hit", () => {
+    // Simulate first ship in coordinates [2,2]
+    gameboard.board[2][2] = 2;
+    gameboard.receiveAttack(2, 2);
+    expect(gameboard.ships[1].hits).toBe(1);
+    gameboard.board[2][3] = 2;
+    gameboard.receiveAttack(2, 3);
+    expect(gameboard.ships[1].hits).toBe(2);
+  });
+  it("All missed shots are recorded", () => {
+    gameboard.receiveAttack(1, 1);
+    gameboard.receiveAttack(2, 2);
+    gameboard.receiveAttack(3, 3);
+    expect(gameboard.missedShots).toEqual([
+      [1, 1],
+      [2, 2],
+      [3, 3],
+    ]);
+  });
+  it("Report when all ships sunk", () => {
+    gameboard.board[0][0] = 1;
+    gameboard.board[0][2] = 1;
+    gameboard.board[0][3] = 2;
+    gameboard.board[0][4] = 2;
+    gameboard.board[0][5] = 2;
+    gameboard.board[0][6] = 3;
+    gameboard.board[0][7] = 3;
+    gameboard.board[0][8] = 3;
+    gameboard.board[0][9] = 4;
+    gameboard.board[1][0] = 4;
+    gameboard.board[2][0] = 4;
+    gameboard.board[3][0] = 4;
+    gameboard.board[4][0] = 5;
+    gameboard.board[5][0] = 5;
+    gameboard.board[6][0] = 5;
+    gameboard.board[7][0] = 5;
+    gameboard.board[8][0] = 5;
+    gameboard.receiveAttack(0, 0);
+    gameboard.receiveAttack(0, 2);
+    gameboard.receiveAttack(0, 3);
+    gameboard.receiveAttack(0, 4);
+    gameboard.receiveAttack(0, 5);
+    gameboard.receiveAttack(0, 6);
+    gameboard.receiveAttack(0, 7);
+    gameboard.receiveAttack(0, 8);
+    gameboard.receiveAttack(0, 9);
+    gameboard.receiveAttack(1, 0);
+    gameboard.receiveAttack(2, 0);
+    gameboard.receiveAttack(3, 0);
+    gameboard.receiveAttack(4, 0);
+    gameboard.receiveAttack(5, 0);
+    gameboard.receiveAttack(6, 0);
+    gameboard.receiveAttack(7, 0);
+    gameboard.receiveAttack(8, 0);
+    expect(gameboard.allShipsSunk).toBeTruthy();
   });
 });
