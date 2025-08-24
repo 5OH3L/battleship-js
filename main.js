5b8357fd7bdc6b85ce39d3a23542ae7effcc65c4
Sohel <152502004+5OH3L@users.noreply.github.com>
2025-08-24 23:22:54 +0530

    feat: Add UI click to register shots

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ import DOM from "./src/scripts/dom.js";
 
 DOM.renderPlayerBoard(player);
 DOM.renderComputerBoard(computer);
+DOM.startGame()
--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,19 +1,41 @@
-const container = document.getElementById("container");
-const playerContainer = document.getElementById("player-container");
 const playerBoard = document.getElementById("player-board");
-const computerContainer = document.getElementById("computer-container");
 const computerBoard = document.getElementById("computer-board");
+const playerTurn = document.getElementById("player-turn");
+
+let isPlayerTurn;
+
+function refreshCells(player, cells) {
+  cells.forEach(cell => {
+    const board = player.gameboard.board;
+    const x = Number(cell.dataset.row);
+    const y = Number(cell.dataset.column);
+    if (board[x][y] === -1 || board[x][y] === -2) {
+      cell.classList.add("shot");
+    }
+  });
+}
 
 function renderBoard(player, DOMBoard) {
+  DOMBoard.innerHTML = "";
   for (let i = 0; i < player.gameboard.board.length; i++) {
     for (let j = 0; j < player.gameboard.board[i].length; j++) {
+      const cellValue = player.gameboard.board[i][j];
       const cell = document.createElement("div");
       cell.className = `cell row${i} column${j}`;
       [1, 2, 3, 4, 5].forEach(shipIndex => {
-        if (player.gameboard.board[i][j] === shipIndex) cell.classList.add("ship", `ship${shipIndex}`);
+        if (cellValue === shipIndex) cell.classList.add("ship", `ship${shipIndex}`);
       });
+      if (cellValue === -1 || cellValue === -2) {
+        cell.classList.add("shot");
+        cell.dataset.shot = "";
+      }
       cell.dataset.row = i;
       cell.dataset.column = j;
+      cell.addEventListener("click", () => {
+        if (cell.dataset.shot === "") return;
+        player.gameboard.receiveAttack(i, j);
+        refreshCells(player, [cell]);
+      });
       DOMBoard.appendChild(cell);
     }
   }
@@ -24,7 +46,18 @@ function renderPlayerBoard(player) {
 function renderComputerBoard(player) {
   renderBoard(player, computerBoard);
 }
+
+function displayTurn(isPlayerTurn) {
+  playerTurn.textContent = `${isPlayerTurn ? "Player" : "Computer"}'s Turn`;
+}
+
+function startGame() {
+  isPlayerTurn = Math.random() < 0.5;
+  displayTurn(isPlayerTurn);
+}
+
 const DOM = {
+  startGame,
   renderPlayerBoard,
   renderComputerBoard,
 };
