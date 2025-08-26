45711b8c8e6b56646f20140945038191cf2de589
Sohel <152502004+5OH3L@users.noreply.github.com>
2025-08-26 17:18:36 +0530

    Remove battleship module
    Removes the battleship module because its logic is being refactored and moved into the dom module, centralizing the handling of UI-related components

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,4 +1,29 @@
-import { computer, player } from "../../battleship";
+import Player from "./player.js";
+
+function initializePlayers() {
+  const player = Player("Player");
+  const computer = Player("Computer");
+
+  // Populate player's gameboard
+  player.gameboard.placeShip([1, 1], 0, true);
+  player.gameboard.placeShip([5, 3], 1);
+  player.gameboard.placeShip([5, 5], 2);
+  player.gameboard.placeShip([7, 3], 3, true);
+  player.gameboard.placeShip([7, 8], 4);
+  console.table(player.gameboard.board);
+
+  // Populate computer's gameboard
+  computer.gameboard.placeShip([3, 9], 0);
+  computer.gameboard.placeShip([6, 7], 1);
+  computer.gameboard.placeShip([8, 3], 2, true);
+  computer.gameboard.placeShip([4, 2], 3, true);
+  computer.gameboard.placeShip([1, 1], 4, true);
+  console.table(computer.gameboard.board);
+  return [player, computer];
+}
+
+let [player, computer] = initializePlayers();
+console.log(player, computer);
 
 const playerBoard = document.getElementById("player-board");
 const computerBoard = document.getElementById("computer-board");
